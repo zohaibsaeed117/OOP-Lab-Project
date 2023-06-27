fc728d1316b68056451aea641c05db005a231781
PsychoZexta <zohaib.saeed1259@gmail.com>
2023-06-27 22:39:22 +0500

    modified the search book feature

--- searchBook.cpp
+++ searchBook.cpp
@@ -7,53 +7,59 @@
 using namespace std;
 void Person::searchBook()
 {
-    system("cls");
-    string bookName;
-    cout << "\n\n\n\tWhat book do you want to search?\t";
-    cin.ignore();
-    getline(cin, bookName);
-    bool flag = false;
-    fstream book;
-    book.open("book.txt", ios::in);
-    while (!book.eof())
+    char choice;
+    do
     {
+        system("cls");
+        string bookName;
+        cout << "\n\n\n\tWhat book do you want to search?\t";
+        cin.ignore();
+        getline(cin, bookName);
+        int flag = 0;
+        fstream book;
+        book.open("book.txt", ios::in);
         string checkBook;
-        getline(book, checkBook);
-        if (bookName == checkBook)
+        while (getline(book, checkBook))
+        {
+            if (bookName == checkBook)
+            {
+                flag = 1;
+                break;
+            }
+            else if (search(checkBook, bookName) > -1)
+            {
+                flag = 2;
+                break;
+            }
+        }
+        book.close();
+        if (flag == 1)
         {
             cout << "\n\n\n\tBook "
                  << "\"" << bookName << "\""
                  << " found" << endl;
-            flag = true;
-            break;
         }
-    }
-    book.close();
-    if (!flag)
-    {
-        cout << "\n\n\n\tBook Not Found!" << endl;
-    }
-    int choice = 0;
-    cout << "\n\n\n\tPress 1 to find another Book" << endl;
-    cout << "\n\n\n\tPress 2 to go to back to the interface" << endl;
-    cout << "\n\n\n\tPress 3 to exit the Program-->" << endl;
-    cout << "\n\n\n\t Enter your Choice--->";
-    cin >> choice;
-    if (choice == 1)
-    {
-        this->searchBook();
-    }
-    else if (choice == 2)
-    {
-        cout << "\n\n\n\tRedirecting to Menu..." << endl;
-        sleep(3);
-        system("cls");
-        this->homePage();
-    }
-    else
-    {
-        cout << "Thanks for using our program!" << endl;
-        system("Pause");
-        system("exit");
-    }
+        else if (flag == 2)
+        {
+            char check;
+            cout << "Did you mean \"" << checkBook << "\" ?(y/n)" << endl;
+            cin >> check;
+            if (check == 'y')
+            {
+                cout << "\n\n\n\tBook "
+                     << "\"" << checkBook << "\""
+                     << " found" << endl;
+            }
+            else
+            {
+                cout << "\n\n\n\tBook Not Found!" << endl;
+            }
+        }
+        else
+        {
+            cout << "\n\n\n\tBook Not Found!" << endl;
+        }
+        cout << "\n\n\n\tDo you want to find another book?";
+        cin >> choice;
+    } while (choice == 'y');
 }
